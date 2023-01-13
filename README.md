0b28afae89a621e54678a904689e7037cf399d0b
Soibjanov001 <122598640+Soibjanov001@users.noreply.github.com>
2023-01-13 16:37:34 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Soibjanov_OS_Project
-My OS Project.
+# OSProject
+
+Basic Client and Server project.
+
+Multiple clients can connect to the server. 
+
+Clients can send files to the server and they can download it from the server.
+
+Clients can chat together via Server.
+
---
+++ client.py
@@ -0,0 +1,73 @@
+import socket
+
+HEADER = 64
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "DISCONNECT!"
+CONNECT_MESSAGE = "CONNECTED!"
+PORT = 5678
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = ('172.20.10.2', 5678)
+
+status = True
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+while status:
+    connected = False
+    command = input("Write a command (please type connect or quit): ")
+    command_split = command.split()
+    com1 = command_split[0]
+
+    if com1 == 'connect':
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(ADDR)
+        send(CONNECT_MESSAGE)
+        send("Hello Server! I am a client.\n")
+        connected = True
+    elif com1 == 'quit':
+        status = False
+    else:
+        print("Error: Command not found")
+
+    while connected:
+        print("\nYou are connected to the server. List of commands: disconnect, quit, lu, send, lf, read, write.")
+        command = input("Write a command: ")
+        command_split = command.split()
+        com1 = command_split[0]
+
+        if com1 == 'disconnect':
+            send(DISCONNECT_MESSAGE)
+            connected = False
+        elif com1 == 'quit':
+            status = False
+            connected = False
+        elif com1 == 'lu':
+            print("List of users...")
+        elif com1 == 'send':
+            send('Message')
+        elif command == 'lf':
+            print("List Files")
+        elif com1 == 'read':
+            if command_split[1] == 'exists':
+                print('Error: Client already consists that file.')
+                continue
+            send('READ')
+        elif com1 == 'write':
+            if command_split[1] == 'exists':
+                print('Error: Client already consists that file.')
+                continue
+                send('WRITE')
+
+        else:
+            print("Error: Command not found!")
+
+print("Disconnected...")
